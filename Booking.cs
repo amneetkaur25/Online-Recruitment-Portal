1ab8d95eccd62250230f2c6817fb8c5639f47b53
Amneet Kaur <111182139+amneetkaur25@users.noreply.github.com>
2023-12-31 22:09:55 +0530

    Add files via upload

---
+++ Booking.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AmneetTest2.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "Please enter a valid name with alphabetic characters only.")]
+        public string CustomerName { get; set; }
+        [Required]
+        public string PickupLocation { get; set; }
+
+        [Required]
+        public string DropOffLocation { get; set; }
+        [Required]
+        public DateTime BookingTime { get; set; }
+        [Required]
+        public int TaxiId { get; set; }
+        //[Display(Name ="Taxi")]
+        //public virtual int TaxiId { get; set; }
+
+        //[ForeignKey("TaxiId")]
+        //public virtual Taxi Taxi { get; set; }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmneetTest2.Models
+{
+    public class Taxi
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Za-z\s]*$", ErrorMessage = "Please enter a valid name with alphabetic characters only.")]
+        public string DriverName { get; set; }
+        [Required]
+        public string CarModel { get; set; }
+
+        [Required]
+        public string LicensePlate { get; set; }
+
+        [Required]
+        public bool IsAvailable { get; set; } = true;
+
+    }
+}
